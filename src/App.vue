4be3284159dd840db6ee15cc1f512a7d506ca7e3
Mist <hatersparty@gmail.com>
2018-10-29 22:54:34 +0300

    [init] project files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <div class="full-width center-content">
+    <h1>Hello Vue.js!</h1>
+  </div>
+  <div class="full-width center-content">
+    <h1>{{ date }}</h1>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    date: Date(),
+    timer: null
+  }),
+  created: function() {
+    this.timer = setInterval(() => (this.date = Date()), 1000);
+  },
+  destroyed: function() {
+    clearInterval(this.timer);
+  }
+};
+</script>
+
+
+<style scoped>
+.full-width {
+  width: 100%;
+}
+.center-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: (process.env.NODE_ENV || 'production').trim(),
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        })
+    ]
+}
